1bc59cbc95e61cf442369ef42444be361836ed69
karunakarmedam <karunakarreddy210@gmail.com>
2022-02-11 16:01:30 +0530

    firstcommit

---
+++ src/main/java/com/example/demo/java/Vendor.java
@@ -0,0 +1,16 @@
+package com.example.demo.java;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Component("vob")
+@Setter
+@Getter
+@ToString
+public class Vendor {
+	private Integer vid;
+	private String vcode;
+
+}
